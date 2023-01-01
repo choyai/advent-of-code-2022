337aa80d0180a68eaa77fe6f0c91abf607240746
choyai <choyaichaiyo@gmail.com>
2023-01-01 15:55:06 +0700

    day 4

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -7,10 +7,12 @@ object App extends ZIOAppDefault {
       day1 <- advent.Day01.solve
       day2 <- advent.Day02.solve
       day3 <- advent.Day03.solve
+      day4 <- advent.Day04.solve
 
       _ <- printLine(s"day 1's answers: ${day1}")
       _ <- printLine(s"day 2's answers: ${day2}")
       _ <- printLine(s"day 3's answers: ${day3}")
+      _ <- printLine(s"day 4's answers: ${day4}")
     } yield 0
 
   def run: ZIO[Environment with ZIOAppArgs with Scope,Any,Any] =
